e27a873053af45ac3066b74f8e188590417b9048
Micky5991 <micky5991@gmail.com>
2018-10-11 17:47:43 +0200

    Remove testing events in Plugin

--- src/RageMP.Net/Plugin.cs
+++ src/RageMP.Net/Plugin.cs
@@ -32,9 +32,6 @@ namespace RageMP.Net
                 { EntityType.Player, PlayerPool },
                 { EntityType.Vehicle, VehiclePool }
             };
-
-            MP.Events.PlayerChat += OnPlayerChat;
-            MP.Events.PlayerEnterVehicle += PlayerEnterVehicle;
         }
 
         internal IEntity BuildEntity(EntityType type, IntPtr entityPointer)
@@ -49,36 +46,6 @@ namespace RageMP.Net
 
                 default:
                     return null;
-            }
-        }
-
-        private void PlayerEnterVehicle(IPlayer player, IVehicle vehicle, uint seat)
-        {
-            player.OutputChatBox($"Du hast das Fahrzeug {vehicle.Model} betreten! (Sitz: {seat}");
-        }
-
-        private void OnPlayerChat(IPlayer player, string text)
-        {
-            Console.WriteLine($"{nameof(OnPlayerChat)}: {player.Name}, {text}, {player.Position}");
-
-            player.GiveWeapon(0x97EA20B8, 100);
-
-            //player.Call("TEST", "cello", 1, 1.2, new Vector3(-1388.594f, -586.711f, 30.219f));
-
-            var vehicle = MP.Vehicles.New(2364918497, player.Position, player.Rotation.Z, "LEL", 255, false, false, uint.MaxValue);
-
-            Console.WriteLine($"Dimension: P: {player.Dimension} - V: {vehicle.Dimension}");
-
-            player.Position = vehicle.Position;
-            player.Dimension = vehicle.Dimension;
-
-            player.PutIntoVehicle(vehicle, -1);
-
-            MP.Players.Broadcast("LEL");
-
-            foreach (var otherPlayer in MP.Players)
-            {
-                otherPlayer.OutputChatBox("HEH");
             }
         }
     }
