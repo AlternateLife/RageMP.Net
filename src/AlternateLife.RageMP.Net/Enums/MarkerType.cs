11195259a8ea25bcdbcfe4f5fed95b2340e6dc14
Micky5991 <micky5991@gmail.com>
2018-10-23 20:33:24 +0200

    Add MarkerType enum

---
+++ src/AlternateLife.RageMP.Net/Enums/MarkerType.cs
@@ -0,0 +1,38 @@
+namespace AlternateLife.RageMP.Net.Enums
+{
+    public enum MarkerType : uint
+    {
+        UpsideDownCone = 0,
+        VerticalCylinder = 1,
+        ThickCevronUp = 2,
+        ThinCevronUp = 3,
+        CheckeredFlagRect = 4,
+        CheckeredFlagCircle = 5,
+        VerticalCircle = 6,
+        PlaneModel = 7,
+        LostMcDark = 8,
+        LostMcLight = 9,
+        Number0 = 10,
+        Number1 = 11,
+        Number2 = 12,
+        Number3 = 13,
+        Number4 = 14,
+        Number5 = 15,
+        Number6 = 16,
+        Number7 = 17,
+        Number8 = 18,
+        Number9 = 19,
+        ChevronUpX1 = 20,
+        ChevronUpX2 = 21,
+        ChevronUpX3 = 22,
+        HorizontalCircleFlat = 23,
+        ReplayIcon = 24,
+        HorizontalCircleSkinny = 25,
+        HorizontalCircleArrow = 26,
+        HorizontalSplitArrowCircle = 27,
+        DebugSphere = 28,
+        Dollar = 29,
+        Stripes = 30,
+        Wolf = 31,
+    }
+}
