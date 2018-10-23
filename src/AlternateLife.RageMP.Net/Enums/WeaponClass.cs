dcd0028be49395d7edd9a0a81ea90948fcb0fe54
Markus Grigull <web@grigull.me>
2018-10-23 20:08:20 +0200

    Add missing weapon enums

---
+++ src/AlternateLife.RageMP.Net/Enums/WeaponClass.cs
@@ -0,0 +1,22 @@
+namespace AlternateLife.RageMP.Net.Enums
+{
+    public enum WeaponClass
+    {
+        Unknown = -1,
+        Unarmed,
+        Parachute,
+        NightVision,
+        Melee,
+        PetrolCan,
+        Thrown,
+        Stungun,
+        FireExtinguisher,
+        Pistol,
+        AssaultRifle,
+        Smg,
+        Mg,
+        Shotgun,
+        Sniper,
+        Heavy
+    }
+}
