d34e9587e6b8173d68885ff39aeda017c63152eb
Micky5991 <micky5991@gmail.com>
2018-10-24 21:12:37 +0200

    Add DeathReason enum

---
+++ src/AlternateLife.RageMP.Net/Enums/DeathReason.cs
@@ -0,0 +1,25 @@
+namespace AlternateLife.RageMP.Net.Enums
+{
+    public enum DeathReason : uint
+    {
+        Briefcase02 = 28811031,
+        RammedByCar = 133987706,
+        Cougar = 148160082,
+        AirstrikeRocket = 324506233,
+        HeliCrash = 341774354,
+        Explosion = 539292904,
+        Exhaustion = 910830060,
+        BarbedWire = 1223143800,
+        DrowningInVehicle = 1936677264,
+        Briefcase = 2294779575,
+        Bleeding = 2339582971,
+        ElectricFence = 2461879995,
+        RunOverByCar = 2741846334,
+        VehicleRocket = 3204302209,
+        HitByWaterCannon = 3425972830,
+        Fall = 3452007600,
+        Fire = 3750660587,
+        Animal = 4194021054,
+        Drowning = 4284007675,
+    }
+}
