a9c03282f59383fd52467f990a1fb1e436c40bf4
Micky5991 <micky5991@gmail.com>
2018-11-29 01:38:07 +0100

    Change type of weather parameters and properties to WeatherType

---
+++ src/AlternateLife.RageMP.Net/Enums/WeatherType.cs
@@ -0,0 +1,22 @@
+namespace AlternateLife.RageMP.Net.Enums
+{
+    public enum WeatherType
+    {
+        Invalid = -1,
+        Extrasunny,
+        Clear,
+        Clouds,
+        Smog,
+        Foggy,
+        Overcast,
+        Rain,
+        Thunder,
+        Clearing,
+        Neutral,
+        Snow,
+        Blizzard,
+        Snowlight,
+        Xmas,
+        Halloween
+    }
+}
