58c7f956721e7ad0abdf0899f255f3c2641e5acc
Micky5991 <micky5991@gmail.com>
2018-11-30 02:07:00 +0100

    Add command execution to CommandParser

--- src/AlternateLife.RageMP.Net/Interfaces/ICommands.cs
+++ src/AlternateLife.RageMP.Net/Interfaces/ICommands.cs
@@ -1,9 +1,18 @@
 using System;
+using AlternateLife.RageMP.Net.Scripting;
 
 namespace AlternateLife.RageMP.Net.Interfaces
 {
     public interface ICommands
     {
+        /// <summary>
+        /// Registers a command directly without dedicated handler class.
+        /// </summary>
+        /// <param name="name">name of the command</param>
+        /// <param name="callback">callback to call when command was executed</param>
+        /// <returns>true if command was registered, false otherwise</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="name"/> is null or empty or <paramref name="callback"/> is null</exception>
+        bool Register(string name, CommandDelegate callback);
 
         /// <summary>
         /// Registers a new commandhandler with command methods
