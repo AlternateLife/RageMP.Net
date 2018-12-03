c1d7c4c7eb2f1a12ae8575fbae34e9b024903fd7
Micky5991 <micky5991@gmail.com>
2018-12-03 18:14:15 +0100

    Improve example resource and move event registration to constructor

--- src/AlternateLife.RageMP.Net.Example/Server/ExampleResource.cs
+++ src/AlternateLife.RageMP.Net.Example/Server/ExampleResource.cs
@@ -1,7 +1,4 @@
-using System.Numerics;
 using System.Threading.Tasks;
-using AlternateLife.RageMP.Net.Data;
-using AlternateLife.RageMP.Net.Enums;
 using AlternateLife.RageMP.Net.Interfaces;
 using AlternateLife.RageMP.Net.Scripting;
 
@@ -9,26 +6,29 @@ namespace AlternateLife.RageMP.Net.Example
 {
     public class ExampleResource : IResource
     {
-        public async Task OnStartAsync()
+        public ExampleResource()
         {
-            MP.Logger.Info($"{nameof(ExampleResource)}: {nameof(OnStartAsync)}");
-
-            MP.Events.PlayerChat += OnPlayerChat;
             MP.Events.PlayerJoin += OnPlayerJoin;
             MP.Events.PlayerReady += OnPlayerReady;
 
             MP.Events.PlayerDeath += OnPlayerDeath;
 
-            var vehicle = await MP.Vehicles.NewAsync(VehicleHash.T20, Vector3.One);
-
-            vehicle.SetColorRgb(new ColorRgba(255, 0, 255), new ColorRgba(0, 125, 25));
-
             MP.Commands.RegisterHandler(new CommandHandler());
+        }
+
+        public Task OnStartAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnStopAsync()
+        {
+            return Task.CompletedTask;
         }
 
         private Task OnPlayerJoin(IPlayer player)
         {
-            MP.Logger.Info($"Player {player.SocialClubName} ({player.Ip}) joined");
+            MP.Logger.Info($"Player {player.SocialClubName} ({player.Ip}) joined!");
 
             return Task.CompletedTask;
         }
@@ -37,12 +37,7 @@ namespace AlternateLife.RageMP.Net.Example
         {
             player.Dimension = MP.GlobalDimension;
 
-            return Task.CompletedTask;
-        }
-
-        public Task OnStopAsync()
-        {
-            MP.Logger.Info($"{nameof(ExampleResource)}: {nameof(OnStopAsync)}");
+            MP.Logger.Info($"Player {player.SocialClubName} ({player.Ip}) is ready now.");
 
             return Task.CompletedTask;
         }
@@ -50,17 +45,6 @@ namespace AlternateLife.RageMP.Net.Example
         private Task OnPlayerDeath(IPlayer player, uint reason, IPlayer killerplayer)
         {
             player.Spawn(player.Position, player.Heading);
-
-            return Task.CompletedTask;
-        }
-
-        private Task OnPlayerChat(IPlayer player, string text)
-        {
-            MP.Markers.NewAsync(MarkerType.PlaneModel, player.Position, Vector3.Zero, Vector3.UnitZ, 1, new ColorRgba(255, 255, 0), true);
-
-            MP.Objects.NewAsync(MP.Joaat("prop_coffin_01"), player.Position, Vector3.One);
-
-            MP.TextLabels.NewAsync(player.Position, "HÖHÖ, höhö", 0, new ColorRgba(255, 255, 0), 20, true);
 
             return Task.CompletedTask;
         }
