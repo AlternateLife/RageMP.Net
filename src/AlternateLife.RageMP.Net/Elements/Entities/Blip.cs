e9793cd3b6141d60ee859f06883daa186aadf7f5
Timschuu <timschu3@gmail.com>
2018-12-06 00:09:46 +0100

    add string validation to blips name and vehicles numberplate

--- src/AlternateLife.RageMP.Net/Elements/Entities/Blip.cs
+++ src/AlternateLife.RageMP.Net/Elements/Entities/Blip.cs
@@ -100,6 +100,7 @@ namespace AlternateLife.RageMP.Net.Elements.Entities
             }
             set
             {
+                Contract.NotNull(value, nameof(value));
                 CheckExistence();
 
                 using (var converter = new StringConverter())
