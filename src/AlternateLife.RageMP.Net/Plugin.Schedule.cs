1b67e15e5e3af3aff78dad51d27aad6a1bad1a81
Micky5991 <micky5991@gmail.com>
2018-12-10 15:08:16 +0100

    Move scheduling into separate partial and restructure scheduler access

---
+++ src/AlternateLife.RageMP.Net/Plugin.Schedule.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlternateLife.RageMP.Net
+{
+    internal partial class Plugin
+    {
+        internal Task Schedule(Action action, bool forceSchedule = false)
+        {
+            if (forceSchedule == false && IsInMainThread())
+            {
+                try
+                {
+                    action();
+
+                    return Task.CompletedTask;
+                }
+                catch (Exception e)
+                {
+                    return Task.FromException(e);
+                }
+            }
+
+            return Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.DenyChildAttach, _taskScheduler);
+        }
+
+        internal Task<T> Schedule<T>(Func<T> action, bool forceSchedule = false)
+        {
+            if (forceSchedule == false && IsInMainThread())
+            {
+                try
+                {
+                    return Task.FromResult(action());
+                }
+                catch (Exception e)
+                {
+                    return Task.FromException<T>(e);
+                }
+            }
+
+            return Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.DenyChildAttach, _taskScheduler);
+        }
+
+        internal void ScheduleTick()
+        {
+            _taskScheduler.Tick();
+        }
+
+        public bool IsInMainThread()
+        {
+            return Thread.CurrentThread.ManagedThreadId == _mainThreadId;
+        }
+    }
+}
