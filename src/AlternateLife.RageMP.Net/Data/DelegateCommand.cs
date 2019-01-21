e79b540b419e9d0a4d513c5ff61b8d71bc214787
Timschuu <timschu3@gmail.com>
2019-01-21 20:41:41 +0100

    Add backward compatibility by handling commands differently based on their signature

---
+++ src/AlternateLife.RageMP.Net/Data/DelegateCommand.cs
@@ -0,0 +1,15 @@
+﻿using AlternateLife.RageMP.Net.Interfaces;
+using AlternateLife.RageMP.Net.Scripting;
+
+namespace AlternateLife.RageMP.Net.Data
+{
+    internal class DelegateCommand : CommandInformation
+    {
+        public CommandDelegate CommandDelegate { get; }
+
+        public DelegateCommand(string name, CommandDelegate commandDelegate, ICommandHandler handler = default(ICommandHandler)) : base(name, handler)
+        {
+            CommandDelegate = commandDelegate;
+        }
+    }
+}
