af8c2ccf9e9e27cc7c59a0930c5878dd62b2bfa6
Francesco Paolocci <micky5991@gmail.com>
2019-01-29 01:49:31 +0100

    Improve and cleanup commandhandler and typeparser

--- src/AlternateLife.RageMP.Net/Scripting/ScriptingClasses/TypeParserRegister.cs
+++ src/AlternateLife.RageMP.Net/Scripting/ScriptingClasses/TypeParserRegister.cs
@@ -9,21 +9,31 @@ namespace AlternateLife.RageMP.Net.Scripting.ScriptingClasses
     {
         public static IEnumerable<KeyValuePair<Type, ITypeParser>> GetStringParsers()
         {
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(int), new PrimitiveParser<int>(int.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(float), new PrimitiveParser<float>(float.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(double), new PrimitiveParser<double>(double.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(uint),    new PrimitiveParser<uint>(uint.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(long), new PrimitiveParser<long>(long.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(char), new PrimitiveParser<char>(char.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(bool), new PrimitiveParser<bool>(bool.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(byte), new PrimitiveParser<byte>(byte.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(sbyte), new PrimitiveParser<sbyte>(sbyte.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(short), new PrimitiveParser<short>(short.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(decimal), new PrimitiveParser<decimal>(decimal.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(ushort), new PrimitiveParser<ushort>(ushort.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(ulong), new PrimitiveParser<ulong>(ulong.TryParse));
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(string), new StringParser());
-            yield return new KeyValuePair<Type, ITypeParser>(typeof(Enum), new EnumParser());
+            KeyValuePair<Type, ITypeParser> AddParserDelegate<T>(ParserDelegate<T> parserDelegate)
+            {
+                return AddParser<T>(new PrimitiveParser<T>(parserDelegate));
+            }
+
+            KeyValuePair<Type, ITypeParser> AddParser<T>(ITypeParser parser)
+            {
+                return new KeyValuePair<Type, ITypeParser>(typeof(T), parser);
+            }
+
+            yield return AddParserDelegate<int>(int.TryParse);
+            yield return AddParserDelegate<float>(float.TryParse);
+            yield return AddParserDelegate<double>(double.TryParse);
+            yield return AddParserDelegate<uint>(uint.TryParse);
+            yield return AddParserDelegate<long>(long.TryParse);
+            yield return AddParserDelegate<char>(char.TryParse);
+            yield return AddParserDelegate<bool>(bool.TryParse);
+            yield return AddParserDelegate<byte>(byte.TryParse);
+            yield return AddParserDelegate<sbyte>(sbyte.TryParse);
+            yield return AddParserDelegate<short>(short.TryParse);
+            yield return AddParserDelegate<decimal>(decimal.TryParse);
+            yield return AddParserDelegate<ushort>(ushort.TryParse);
+            yield return AddParserDelegate<ulong>(ulong.TryParse);
+            yield return AddParser<string>(new StringParser());
+            yield return AddParser<Enum>(new EnumParser());
         }
     }
 }
