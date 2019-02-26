bbe53e16206822dd3631b3bc142bdd4df3625c1e
Francesco Paolocci <micky5991@gmail.com>
2019-02-26 13:27:55 +0100

    Extract Native capabilities from EventHandler into separate handler

---
+++ src/AlternateLife.RageMP.Net/Helpers/NativeEventHandler.cs
@@ -0,0 +1,55 @@
+using System.Linq;
+using System.Runtime.InteropServices;
+using AlternateLife.RageMP.Net.Enums;
+using AlternateLife.RageMP.Net.Native;
+
+namespace AlternateLife.RageMP.Net.Helpers
+{
+    internal class NativeEventHandler<TNative, TEvent> : EventHandler<TEvent>
+    {
+        private readonly EventType _type;
+        private readonly TNative _nativeCallback;
+        private readonly bool _forceRegistration;
+
+        public NativeEventHandler(Plugin plugin, EventType type, TNative nativeCallback, bool forceRegistration = false) : base(plugin)
+        {
+            _type = type;
+            _nativeCallback = nativeCallback;
+            _forceRegistration = forceRegistration;
+
+            if (_forceRegistration)
+            {
+                Rage.Events.RegisterEventHandler((int) _type, Marshal.GetFunctionPointerForDelegate(_nativeCallback));
+            }
+        }
+
+        public override bool Subscribe(TEvent callback)
+        {
+            var wasEmpty = _subscriptions.Any() == false;
+            var wasAdded = base.Subscribe(callback);
+
+            if (_forceRegistration || wasAdded == false || wasEmpty == false)
+            {
+                return true;
+            }
+
+            Rage.Events.RegisterEventHandler((int) _type, Marshal.GetFunctionPointerForDelegate(_nativeCallback));
+
+            return true;
+        }
+
+        public override bool Unsubscribe(TEvent callback)
+        {
+            var wasRemoved = base.Unsubscribe(callback);
+
+            if (_forceRegistration || wasRemoved == false || _subscriptions.Any())
+            {
+                return true;
+            }
+
+            Rage.Events.UnregisterEventHandler((int) _type);
+
+            return true;
+        }
+    }
+}
