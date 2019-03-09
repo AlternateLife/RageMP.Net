ad384d1b0ba480058a5837ab2a73004b0bf06e67
Markus Grigull <web@grigull.me>
2019-03-09 20:10:50 +0100

    Replace text label properties with async getter/setters

--- src/AlternateLife.RageMP.Net/Interfaces/ITextLabel.cs
+++ src/AlternateLife.RageMP.Net/Interfaces/ITextLabel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Threading.Tasks;
 using AlternateLife.RageMP.Net.Exceptions;
 
 namespace AlternateLife.RageMP.Net.Interfaces
@@ -7,35 +8,75 @@ namespace AlternateLife.RageMP.Net.Interfaces
     public interface ITextLabel : IEntity
     {
         /// <summary>
-        /// Get or set the color of the text label.
+        /// Set the color of the text label.
         /// </summary>
+        /// <param name="color">New color of the text label</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
-        Color Color { get; set; }
+        Task SetColorAsync(Color color);
 
         /// <summary>
-        /// Get or set the text of the text label.
+        /// Get the color of the text label.
         /// </summary>
+        /// <returns>Color of the text label</returns>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
-        /// <exception cref="ArgumentNullException"><paramref name="value"/> is null</exception>
-        string Text { get; set; }
+        Task<Color> GetColorAsync();
 
         /// <summary>
-        /// Get or set the line of sight state of the text label.
+        /// Set the text of the text label.
         /// </summary>
+        /// <param name="text">New text of the text label</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
-        bool LOS { get; set; }
+        /// <exception cref="ArgumentNullException"><paramref name="text"/> is null</exception>
+        Task SetTextAsync(string text);
 
         /// <summary>
-        /// Get or set the draw distance of the text label.
+        /// Get the text of the text label.
         /// </summary>
+        /// <returns>Text of the text label</returns>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
-        float DrawDistance { get; set; }
+        Task<string> GetTextAsync();
 
         /// <summary>
-        /// Get or set the font of the text label.
+        /// Set the line of sight state of the text label.
         /// </summary>
+        /// <param name="los">New line of sight state of the text label</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
-        uint Font { get; set; }
+        Task SetLOSAsync(bool los);
+
+        /// <summary>
+        /// Get the line of sight state of the text label.
+        /// </summary>
+        /// <returns>Line of sight state of the text label</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        Task<bool> GetLOSAsync();
+
+        /// <summary>
+        /// Set the draw distance of the text label.
+        /// </summary>
+        /// <param name="drawDistance">New draw distance of the text label</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        Task SetDrawDistanceAsync(float drawDistance);
+
+        /// <summary>
+        /// Get the draw distance of the text label.
+        /// </summary>
+        /// <returns>New draw distance of the text label</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        Task<float> GetDrawDistanceAsync();
+
+        /// <summary>
+        /// Set the font of the text label.
+        /// </summary>
+        /// <param name="font">New font of the text label</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        Task SetFontAsync(uint font);
+
+        /// <summary>
+        /// Get the font of the text label.
+        /// </summary>
+        /// <returns>Font of the text label</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        Task<uint> GetFontAsync();
 
     }
 }
