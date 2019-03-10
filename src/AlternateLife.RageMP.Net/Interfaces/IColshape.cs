1c61bb8394af8f1a5a088dbe9d98a05534068198
Francesco Paolocci <micky5991@gmail.com>
2019-03-10 13:01:10 +0100

    Add synchronous api variants to IColshape

--- src/AlternateLife.RageMP.Net/Interfaces/IColshape.cs
+++ src/AlternateLife.RageMP.Net/Interfaces/IColshape.cs
@@ -12,7 +12,22 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <returns>Type of the colshape</returns>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        ColshapeType GetShapeType();
+
+        /// <summary>
+        /// Get the type of the colshape.
+        /// </summary>
+        /// <returns>Type of the colshape</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task<ColshapeType> GetShapeTypeAsync();
+
+        /// <summary>
+        /// Check if a position is within the colshape.
+        /// </summary>
+        /// <param name="position">Position to check</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        /// <returns>True if the <paramref name="position" /> is inside the colshape, otherwise false</returns>
+        bool IsPointWhithin(Vector3 position);
 
         /// <summary>
         /// Check if a position is within the colshape.
