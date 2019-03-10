35cd9a04a3bd6eb158051ffd36e552492e0c3dd0
Francesco Paolocci <micky5991@gmail.com>
2019-03-10 12:55:26 +0100

    Add synchronous api variants to ICheckpoint

--- src/AlternateLife.RageMP.Net/Interfaces/ICheckpoint.cs
+++ src/AlternateLife.RageMP.Net/Interfaces/ICheckpoint.cs
@@ -14,7 +14,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="color">New color of the checkpoint</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetColor(Color color);
+
+        /// <summary>
+        /// Set the color of the checkpoint.
+        /// </summary>
+        /// <param name="color">New color of the checkpoint</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetColorAsync(Color color);
+
+        /// <summary>
+        /// Get the color of the checkpoint.
+        /// </summary>
+        /// <returns>Color of the checkpoint</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        Color GetColor();
 
         /// <summary>
         /// Get the color of the checkpoint.
@@ -28,7 +42,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="direction">New direction of the checkpoint</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetDirection(Vector3 direction);
+
+        /// <summary>
+        /// Set the direction of the checkpoint.
+        /// </summary>
+        /// <param name="direction">New direction of the checkpoint</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetDirectionAsync(Vector3 direction);
+
+        /// <summary>
+        /// Get the direction of the checkpoint.
+        /// </summary>
+        /// <returns>Direction of the checkpoint</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        Vector3 GetDirection();
 
         /// <summary>
         /// Get the direction of the checkpoint.
@@ -42,7 +70,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="radius">Radius of the checkpoint</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetRadius(float radius);
+
+        /// <summary>
+        /// Set the radius of the checkpoint.
+        /// </summary>
+        /// <param name="radius">Radius of the checkpoint</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetRadiusAsync(float radius);
+
+        /// <summary>
+        /// Get the radius of the checkpoint.
+        /// </summary>
+        /// <returns>Radius of the checkpoint</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        float GetRadius();
 
         /// <summary>
         /// Get the radius of the checkpoint.
@@ -56,7 +98,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="visible">New visible state of the checkpoint</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetVisible(bool visible);
+
+        /// <summary>
+        /// Set if the checkpoint is visible.
+        /// </summary>
+        /// <param name="visible">New visible state of the checkpoint</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetVisibleAsync(bool visible);
+
+        /// <summary>
+        /// Get if the checkpoint is visible.
+        /// </summary>
+        /// <returns>Visible state of the checkpoint</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        bool IsVisible();
 
         /// <summary>
         /// Get if the checkpoint is visible.
@@ -71,7 +127,23 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// <param name="players">List of players to show the checkpoint for</param>
         /// <exception cref="ArgumentNullException"><paramref name="players" /> is null</exception>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void ShowFor(IEnumerable<IPlayer> players);
+
+        /// <summary>
+        /// Show checkpoint for a list of players.
+        /// </summary>
+        /// <param name="players">List of players to show the checkpoint for</param>
+        /// <exception cref="ArgumentNullException"><paramref name="players" /> is null</exception>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task ShowForAsync(IEnumerable<IPlayer> players);
+
+        /// <summary>
+        /// Hide checkpoint for a list of players.
+        /// </summary>
+        /// <param name="players">List of players to hide the checkpoint for</param>
+        /// <exception cref="ArgumentNullException"><paramref name="players" /> is null</exception>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void HideFor(IEnumerable<IPlayer> players);
 
         /// <summary>
         /// Hide checkpoint for a list of players.
