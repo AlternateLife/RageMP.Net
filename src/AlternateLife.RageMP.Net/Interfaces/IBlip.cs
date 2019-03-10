e96afbbf7a54909b0e451b88ee07e5fcc5130c81
Francesco Paolocci <micky5991@gmail.com>
2019-03-10 12:29:12 +0100

    Add synchronous api variants to IBlip

--- src/AlternateLife.RageMP.Net/Interfaces/IBlip.cs
+++ src/AlternateLife.RageMP.Net/Interfaces/IBlip.cs
@@ -12,7 +12,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="distance">New draw distance of the blip</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetDrawDistance(float distance);
+        
+        /// <summary>
+        /// Set the draw distance of the blip.
+        /// </summary>
+        /// <param name="distance">New draw distance of the blip</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetDrawDistanceAsync(float distance);
+
+        /// <summary>
+        /// Get the draw distance of the blip.
+        /// </summary>
+        /// <returns>Draw distance of the blip</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        float GetDrawDistance();
 
         /// <summary>
         /// Get the draw distance of the blip.
@@ -26,7 +40,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="rotation">New rotation of the blip</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetRotation(int rotation);
+
+        /// <summary>
+        /// Set the rotation of the blip.
+        /// </summary>
+        /// <param name="rotation">New rotation of the blip</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetRotationAsync(int rotation);
+
+        /// <summary>
+        /// Get the rotation of the blip.
+        /// </summary>
+        /// <returns>Rotation of the blip</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        new int GetRotation();
 
         /// <summary>
         /// Get the rotation of the blip.
@@ -40,7 +68,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="shortRange">New short range flag of the blip</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetShortRange(bool shortRange);
+
+        /// <summary>
+        /// Set the short range flag of the blip.
+        /// </summary>
+        /// <param name="shortRange">New short range flag of the blip</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetShortRangeAsync(bool shortRange);
+
+        /// <summary>
+        /// Get the short range flag of the blip.
+        /// </summary>
+        /// <returns>Short range of the blip</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        bool GetShortRange();
 
         /// <summary>
         /// Get the short range flag of the blip.
@@ -54,7 +96,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="color">Color of the blip</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetColor(uint color);
+
+        /// <summary>
+        /// Set the color of the blip.
+        /// </summary>
+        /// <param name="color">Color of the blip</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetColorAsync(uint color);
+
+        /// <summary>
+        /// Get the color of the blip.
+        /// </summary>
+        /// <returns>Color of the blip</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        uint GetColor();
 
         /// <summary>
         /// Get the color of the blip.
@@ -68,7 +124,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="scale">New scale of the blip</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void SetScale(float scale);
+
+        /// <summary>
+        /// Set the scale of the blip.
+        /// </summary>
+        /// <param name="scale">New scale of the blip</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task SetScaleAsync(float scale);
+
+        /// <summary>
+        /// Get the scale of the blip.
+        /// </summary>
+        /// <returns>Scale of the blip</returns>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        float GetScale();
 
         /// <summary>
         /// Get the scale of the blip.
@@ -83,12 +153,37 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// <param name="name">New name of the blip</param>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         /// <exception cref="ArgumentNullException"><paramref name="name"/> is null</exception>
+        void SetName(string name);
+
+        /// <summary>
+        /// Set the name of the blip.
+        /// </summary>
+        /// <param name="name">New name of the blip</param>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="name"/> is null</exception>
         Task SetNameAsync(string name);
 
         /// <summary>
         /// Get the name of the blip.
         /// </summary>
+        string GetName();
+
+        /// <summary>
+        /// Get the name of the blip.
+        /// </summary>
         Task<string> GetNameAsync();
+
+        /// <summary>
+        /// Show route for a list of players with given color and scale.
+        ///
+        /// The <paramref name="color" /> and <paramref name="scale" /> are used for the route, not the blip.
+        /// </summary>
+        /// <param name="forPlayers">List of players to show the route for</param>
+        /// <param name="color">Color of the route</param>
+        /// <param name="scale">Scale of the route</param>
+        /// <exception cref="ArgumentNullException"><paramref name="forPlayers" /> is null</exception>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void ShowRoute(IEnumerable<IPlayer> forPlayers, uint color, float scale);
 
         /// <summary>
         /// Show route for a list of players with given color and scale.
@@ -112,7 +207,27 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// <param name="scale">Scale of the route</param>
         /// <exception cref="ArgumentNullException"><paramref name="forPlayers" /> is null</exception>
         /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void ShowRoute(IEnumerable<IPlayer> forPlayers, int color, float scale);
+
+        /// <summary>
+        /// Show route for a list of players with given color and scale.
+        ///
+        /// The <paramref name="color" /> and <paramref name="scale" /> are used for the route, not the blip.
+        /// </summary>
+        /// <param name="forPlayers">List of players to show the route for</param>
+        /// <param name="color">Color of the route</param>
+        /// <param name="scale">Scale of the route</param>
+        /// <exception cref="ArgumentNullException"><paramref name="forPlayers" /> is null</exception>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
         Task ShowRouteAsync(IEnumerable<IPlayer> forPlayers, int color, float scale);
+
+        /// <summary>
+        /// Hide route for a list of players.
+        /// </summary>
+        /// <param name="forPlayers">List of players to hide the route for</param>
+        /// <exception cref="ArgumentNullException"><paramref name="forPlayers" /> is null</exception>
+        /// <exception cref="EntityDeletedException">This entity was deleted before</exception>
+        void HideRoute(IEnumerable<IPlayer> forPlayers);
 
         /// <summary>
         /// Hide route for a list of players.
