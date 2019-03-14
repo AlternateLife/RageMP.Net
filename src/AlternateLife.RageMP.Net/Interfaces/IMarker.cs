c32cb6a99b39661c38039ec136c976e23648749c
Markus Grigull <web@grigull.me>
2019-03-14 21:44:19 +0100

    Add synced methods to marker

--- src/AlternateLife.RageMP.Net/Interfaces/IMarker.cs
+++ src/AlternateLife.RageMP.Net/Interfaces/IMarker.cs
@@ -14,7 +14,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="color">New color of the marker</param>
         /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        void SetColor(Color color);
+
+        /// <summary>
+        /// Set the color of the marker.
+        /// </summary>
+        /// <param name="color">New color of the marker</param>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
         Task SetColorAsync(Color color);
+
+        /// <summary>
+        /// Get the color of the marker.
+        /// </summary>
+        /// <returns>Color of the marker</returns>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        Color GetColor();
 
         /// <summary>
         /// Get the color of the marker.
@@ -27,7 +41,20 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// Set the direction of the marker.
         /// </summary>
         /// <param name="direction">New direction of the marker</param>
+        void SetDirection(Vector3 direction);
+
+        /// <summary>
+        /// Set the direction of the marker.
+        /// </summary>
+        /// <param name="direction">New direction of the marker</param>
         Task SetDirectionAsync(Vector3 direction);
+
+        /// <summary>
+        /// Get the direction of the marker.
+        /// </summary>
+        /// <returns>Direction of the marker</returns>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        Vector3 GetDirection();
 
         /// <summary>
         /// Get the direction of the marker.
@@ -41,7 +68,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="scale">New scale of the marker</param>
         /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        void SetScale(float scale);
+
+        /// <summary>
+        /// Set the scale of the marker.
+        /// </summary>
+        /// <param name="scale">New scale of the marker</param>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
         Task SetScaleAsync(float scale);
+
+        /// <summary>
+        /// Get or set the scale of the marker.
+        /// </summary>
+        /// <returns>Scale of the marker</returns>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        float GetScale();
 
         /// <summary>
         /// Get or set the scale of the marker.
@@ -55,7 +96,21 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// </summary>
         /// <param name="visible">New visible state of the marker</param>
         /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        void SetVisible(bool visible);
+
+        /// <summary>
+        /// Set the visible state of the marker.
+        /// </summary>
+        /// <param name="visible">New visible state of the marker</param>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
         Task SetVisibleAsync(bool visible);
+
+        /// <summary>
+        /// Get the visible state of the marker.
+        /// </summary>
+        /// <returns>True if the marker is visible, otherwise false</returns>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        bool IsVisible();
 
         /// <summary>
         /// Get the visible state of the marker.
@@ -70,7 +125,23 @@ namespace AlternateLife.RageMP.Net.Interfaces
         /// <param name="players">List of players to show the marker for</param>
         /// <exception cref="ArgumentNullException"><paramref name="players" /> is null</exception>
         /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        void ShowFor(IEnumerable<IPlayer> players);
+
+        /// <summary>
+        /// Show marker for a list of players.
+        /// </summary>
+        /// <param name="players">List of players to show the marker for</param>
+        /// <exception cref="ArgumentNullException"><paramref name="players" /> is null</exception>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
         Task ShowForAsync(IEnumerable<IPlayer> players);
+
+        /// <summary>
+        /// Hide marker for a list of players.
+        /// </summary>
+        /// <param name="players">List of players to hide the marker for</param>
+        /// <exception cref="ArgumentNullException"><paramref name="players" /> is null</exception>
+        /// <exception cref="EntityDeletedException">This marker was deleted before</exception>
+        void HideFor(IEnumerable<IPlayer> players);
 
         /// <summary>
         /// Hide marker for a list of players.
