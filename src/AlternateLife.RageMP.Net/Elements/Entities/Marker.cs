a073d599fe52ff93504caa515c46d662ed5d70f7
Markus Grigull <web@grigull.me>
2019-03-14 21:44:19 +0100

    Add synced methods to marker

--- src/AlternateLife.RageMP.Net/Elements/Entities/Marker.cs
+++ src/AlternateLife.RageMP.Net/Elements/Entities/Marker.cs
@@ -19,104 +19,128 @@ namespace AlternateLife.RageMP.Net.Elements.Entities
         {
         }
 
-        public async Task SetColorAsync(Color value)
+        public void SetColor(Color value)
         {
             CheckExistence();
 
-            await _plugin
-                .Schedule(() => Rage.Marker.Marker_SetColor(NativePointer, value.R, value.G, value.B, value.A))
-                .ConfigureAwait(false);
+            Rage.Marker.Marker_SetColor(NativePointer, value.R, value.G, value.B, value.A);
         }
 
-        public async Task<Color> GetColorAsync()
+        public Task SetColorAsync(Color value)
+        {
+            return _plugin.Schedule(() => SetColor(value));
+        }
+
+        public Color GetColor()
         {
             CheckExistence();
 
-            var colorPointer = await _plugin
-                .Schedule(() => Rage.Marker.Marker_GetColor(NativePointer))
-                .ConfigureAwait(false);
-
-            return StructConverter.PointerToStruct<ColorRgba>(colorPointer).FromModColor();
+            return StructConverter.PointerToStruct<ColorRgba>(Rage.Marker.Marker_GetColor(NativePointer)).FromModColor();
         }
 
-        public async Task SetDirectionAsync(Vector3 value)
+        public Task<Color> GetColorAsync()
+        {
+            return _plugin.Schedule(GetColor);
+        }
+
+        public void SetDirection(Vector3 value)
         {
             CheckExistence();
 
-            await _plugin
-                .Schedule(() => Rage.Marker.Marker_SetDirection(NativePointer, value))
-                .ConfigureAwait(false);
+            Rage.Marker.Marker_SetDirection(NativePointer, value);
         }
 
-        public async Task<Vector3> GetDirectionAsync()
+        public Task SetDirectionAsync(Vector3 value)
+        {
+            return _plugin.Schedule(() => SetDirection(value));
+        }
+
+        public Vector3 GetDirection()
         {
             CheckExistence();
 
-            var directionPointer = await _plugin
-                .Schedule(() => Rage.Marker.Marker_GetDirection(NativePointer))
-                .ConfigureAwait(false);
-
-            return StructConverter.PointerToStruct<Vector3>(directionPointer);
+            return StructConverter.PointerToStruct<Vector3>(Rage.Marker.Marker_GetDirection(NativePointer));
         }
 
-        public async Task SetScaleAsync(float value)
+        public Task<Vector3> GetDirectionAsync()
+        {
+            return _plugin.Schedule(GetDirection);
+        }
+
+        public void SetScale(float value)
         {
             CheckExistence();
 
-            await _plugin
-                .Schedule(() => Rage.Marker.Marker_SetScale(NativePointer, value))
-                .ConfigureAwait(false);
+            Rage.Marker.Marker_SetScale(NativePointer, value);
         }
 
-        public async Task<float> GetScaleAsync()
+        public Task SetScaleAsync(float value)
+        {
+            return _plugin.Schedule(() => SetScale(value));
+        }
+
+        public float GetScale()
         {
             CheckExistence();
 
-            return await _plugin
-                .Schedule(() => Rage.Marker.Marker_GetScale(NativePointer))
-                .ConfigureAwait(false);
+            return Rage.Marker.Marker_GetScale(NativePointer);
         }
 
-        public async Task SetVisibleAsync(bool value)
+        public Task<float> GetScaleAsync()
+        {
+            return _plugin.Schedule(GetScale);
+        }
+
+        public void SetVisible(bool value)
         {
             CheckExistence();
 
-            await _plugin
-                .Schedule(() => Rage.Marker.Marker_SetVisible(NativePointer, value))
-                .ConfigureAwait(false);
+            Rage.Marker.Marker_SetVisible(NativePointer, value);
         }
 
-        public async Task<bool> IsVisibleAsync()
+        public Task SetVisibleAsync(bool value)
         {
-            CheckExistence();
-
-            return await _plugin
-                .Schedule(() => Rage.Marker.Marker_IsVisible(NativePointer))
-                .ConfigureAwait(false);
+            return _plugin.Schedule(() => SetVisible(value));
         }
 
-        public async Task ShowForAsync(IEnumerable<IPlayer> players)
+        public bool IsVisible()
+        {
+            return Rage.Marker.Marker_IsVisible(NativePointer);
+        }
+
+        public Task<bool> IsVisibleAsync()
+        {
+            return _plugin.Schedule(IsVisible);
+        }
+
+        public void ShowFor(IEnumerable<IPlayer> players)
         {
             Contract.NotNull(players, nameof(players));
             CheckExistence();
 
             var pointers = players.Select(x => x.NativePointer).ToArray();
 
-            await _plugin
-                .Schedule(() => Rage.Marker.Marker_ShowFor(NativePointer, pointers, (ulong) pointers.LongLength))
-                .ConfigureAwait(false);
+            Rage.Marker.Marker_ShowFor(NativePointer, pointers, (ulong) pointers.LongLength);
         }
 
-        public async Task HideForAsync(IEnumerable<IPlayer> players)
+        public Task ShowForAsync(IEnumerable<IPlayer> players)
+        {
+            return _plugin.Schedule(() => ShowFor(players));
+        }
+
+        public void HideFor(IEnumerable<IPlayer> players)
         {
             Contract.NotNull(players, nameof(players));
             CheckExistence();
 
             var pointers = players.Select(x => x.NativePointer).ToArray();
 
-            await _plugin
-                .Schedule(() => Rage.Marker.Marker_HideFor(NativePointer, pointers, (ulong) pointers.LongLength))
-                .ConfigureAwait(false);
+            Rage.Marker.Marker_HideFor(NativePointer, pointers, (ulong) pointers.LongLength);
+        }
+
+        public Task HideForAsync(IEnumerable<IPlayer> players)
+        {
+            return _plugin.Schedule(() => HideFor(players));
         }
     }
 }
