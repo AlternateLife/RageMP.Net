5e4e21df31b7407d6795a3ad816e9e7b8adaa2d8
Francesco Paolocci <micky5991@gmail.com>
2019-03-17 09:04:21 +0100

    Change AsyncEventDispatcher to use event pattern

--- src/AlternateLife.RageMP.Net/Scripting/EventDelegates.cs
+++ src/AlternateLife.RageMP.Net/Scripting/EventDelegates.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Numerics;
 using System.Threading.Tasks;
 using AlternateLife.RageMP.Net.Enums;
@@ -6,6 +7,8 @@ using AlternateLife.RageMP.Net.Scripting.ScriptingClasses;
 
 namespace AlternateLife.RageMP.Net.Scripting
 {
+    public delegate Task AsyncEventHandler<T>(object sender, T e) where T : EventArgs;
+
     public delegate Task TickDelegate();
 
     public delegate Task EntityCreatedDelegate(IEntity entity);
