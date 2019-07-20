de6203265f4692a92decfc9f45b8815771cec2e9
Francesco Paolocci <micky5991@gmail.com>
2019-07-20 18:44:49 +0200

    Improve EventHandling in Colshape

--- src/AlternateLife.RageMP.Net/Elements/Entities/Colshape.cs
+++ src/AlternateLife.RageMP.Net/Elements/Entities/Colshape.cs
@@ -13,29 +13,29 @@ namespace AlternateLife.RageMP.Net.Elements.Entities
     internal class Colshape : Entity, IColshape
     {
 
-        private readonly AsyncChildEventDispatcher<PlayerColshapeEventArgs> _playerEnterColshape;
-        private readonly AsyncChildEventDispatcher<PlayerColshapeEventArgs> _playerExitColshape;
+        private readonly AsyncChildEventDispatcher<PlayerColshapeEventArgs> _playerEnterDispatcher;
+        private readonly AsyncChildEventDispatcher<PlayerColshapeEventArgs> _playerExitDispatcher;
 
         public event AsyncEventHandler<PlayerColshapeEventArgs> PlayerEnter
         {
-            add => _playerEnterColshape.Subscribe(value, out _);
-            remove => _playerEnterColshape.Unsubscribe(value, out _);
+            add => _playerEnterDispatcher.Subscribe(value, out _);
+            remove => _playerEnterDispatcher.Unsubscribe(value, out _);
         }
 
         public event AsyncEventHandler<PlayerColshapeEventArgs> PlayerExit
         {
-            add => _playerExitColshape.Subscribe(value, out _);
-            remove => _playerExitColshape.Unsubscribe(value, out _);
+            add => _playerExitDispatcher.Subscribe(value, out _);
+            remove => _playerExitDispatcher.Unsubscribe(value, out _);
         }
 
         internal Colshape(IntPtr nativePointer, Plugin plugin) : base(nativePointer, plugin, EntityType.Colshape)
         {
             var events = _plugin.EventScripting;
 
-            _playerEnterColshape = new AsyncChildEventDispatcher<PlayerColshapeEventArgs>(_plugin, "PlayerEnterColshape",
+            _playerEnterDispatcher = new AsyncChildEventDispatcher<PlayerColshapeEventArgs>(_plugin, "PlayerEnterColshape",
                 events.PlayerEnterColshapeDispatcher, eventArgs => Task.FromResult(eventArgs.Colshape == this));
 
-            _playerExitColshape = new AsyncChildEventDispatcher<PlayerColshapeEventArgs>(_plugin, "PlayerExitColshape",
+            _playerExitDispatcher = new AsyncChildEventDispatcher<PlayerColshapeEventArgs>(_plugin, "PlayerExitColshape",
                 events.PlayerExitColshapeDispatcher, eventArgs => Task.FromResult(eventArgs.Colshape == this));
         }
 
@@ -65,10 +65,10 @@ namespace AlternateLife.RageMP.Net.Elements.Entities
 
         public override void Destroy()
         {
-            _playerEnterColshape.ClearSubscriptions();
-            _playerExitColshape.ClearSubscriptions();
-
             base.Destroy();
+
+            _playerEnterDispatcher.ClearSubscriptions();
+            _playerExitDispatcher.ClearSubscriptions();
         }
     }
 }
